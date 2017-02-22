069d28453911046600a46fae1e0041d2a59679f6
David <djr1@e3-11-19.ece.ualberta.ca>
2017-02-22 14:36:56 -0700

    Added servo files

---
+++ output_files/servo.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 32-bit Version 12.1 Build 243 01/31/2013 Service Pack 1 SJ Full Version */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP2C35F672) Path("/afs/ualberta.ca/home/d/j/djr1/ece492_projects/ece492_projects/project_servo/output_files/") File("servo.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ software/servo/obj/default/hello_ucosii.d
@@ -0,0 +1,43 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../servo_bsp//HAL/inc/includes.h ../servo_bsp//HAL/inc/os_cpu.h \
+  ../servo_bsp//HAL/inc/sys/alt_irq.h ../servo_bsp//HAL/inc/nios2.h \
+  ../servo_bsp//HAL/inc/alt_types.h ../servo_bsp/system.h \
+  ../servo_bsp/linker.h ../servo_bsp//UCOSII/inc/os_cfg.h \
+  ../servo_bsp//HAL/inc/sys/alt_alarm.h \
+  ../servo_bsp//HAL/inc/sys/alt_llist.h \
+  ../servo_bsp//HAL/inc/priv/alt_alarm.h \
+  ../servo_bsp//HAL/inc/alt_types.h ../servo_bsp/system.h \
+  ../servo_bsp//UCOSII/inc/ucos_ii.h ../servo_bsp//UCOSII/inc/os_cfg.h \
+  ../servo_bsp//HAL/inc/os_cpu.h
+
+../servo_bsp//HAL/inc/includes.h:
+
+../servo_bsp//HAL/inc/os_cpu.h:
+
+../servo_bsp//HAL/inc/sys/alt_irq.h:
+
+../servo_bsp//HAL/inc/nios2.h:
+
+../servo_bsp//HAL/inc/alt_types.h:
+
+../servo_bsp/system.h:
+
+../servo_bsp/linker.h:
+
+../servo_bsp//UCOSII/inc/os_cfg.h:
+
+../servo_bsp//HAL/inc/sys/alt_alarm.h:
+
+../servo_bsp//HAL/inc/sys/alt_llist.h:
+
+../servo_bsp//HAL/inc/priv/alt_alarm.h:
+
+../servo_bsp//HAL/inc/alt_types.h:
+
+../servo_bsp/system.h:
+
+../servo_bsp//UCOSII/inc/ucos_ii.h:
+
+../servo_bsp//UCOSII/inc/os_cfg.h:
+
+../servo_bsp//HAL/inc/os_cpu.h:
